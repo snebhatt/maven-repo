234f5df1d1a4b787b36b6ffcdfced8882990a683
snebhatt <51321683+snebhatt@users.noreply.github.com>
2024-02-05 02:43:14 +0100

    Code Red. AEM tasks

--- AemPracticeModel.java
+++ AemPracticeModel.java
@@ -3,34 +3,34 @@ package com.adobe.aem.task.core.models;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
- 
+
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.List;
 
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class AemPracticeModel {
-	
-	@Inject
-    private String imagePath;
- 
-    @Inject
-    private String isBackground;
- 
-    @Inject
-    @Named("multifield")
-    private List<NavigationItemsModel> navList;
- 
-    public String getSubtitle() {
-        return imagePath;
-    }
- 
-    public boolean isConfigured() {
-    	return Boolean.valueOf(isBackground);
-    }
 
-    public List<NavigationItemsModel> getNavList() {
-        return navList;
-    }
- 
-    
+	@Inject
+	private String imagePath;
+
+	@Inject
+	private String isBackground;
+
+	@Inject
+	@Named("multifield")
+	private List<NavigationItemsModel> navList;
+
+	public String getSubtitle() {
+		return imagePath;
+	}
+
+	public boolean isConfigured() {
+		return Boolean.valueOf(isBackground);
+	}
+
+	public List<NavigationItemsModel> getNavList() {
+		return navList;
+	}
+
 }
